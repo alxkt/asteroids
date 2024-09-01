84b7e0d0bb9dd551748c2922d015510a3ee2d464
alxkt <alexkturnbull@gmail.com>
2024-09-01 16:18:32 -0700

    drawing a triangle player and rotating with keys

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pygame
 from constants import *
-
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    dt = 0
+    clock = pygame.time.Clock()
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -12,7 +17,12 @@ def main():
                 return
 
         screen.fill("black")
+
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":

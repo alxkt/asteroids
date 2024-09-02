7cbf2809d0a2d9870ebe41cfab2d9a38eb590b47
alxkt <alexkturnbull@gmail.com>
2024-09-01 17:46:32 -0700

    added collision

--- circleshape.py
+++ circleshape.py
@@ -13,6 +13,13 @@ class CircleShape(pygame.sprite.Sprite):
     self.position = pygame.Vector2(x, y)
     self.velocity = pygame.Vector2(0, 0)
     self.radius = radius
+  
+  def collides_with(self, other):
+    distance = self.position.distance_to(other.position)
+    if distance <= self.radius + other.radius:
+      return True
+    else:
+      return False
 
   def draw(self, screen):
     pass
